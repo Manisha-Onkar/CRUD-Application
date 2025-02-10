3d09ed745f150373b4421220465337696ba65577
manisha <onkarmanisha30@gmail.com>
2025-02-10 08:27:52 +0530

    Description of changes

--- react-crud/src/index.tsx
+++ react-crud/src/index.tsx
@@ -1,17 +1,39 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import Home from "./component/Home";
+// import './index.css';
+// import reportWebVitals from './reportWebVitals';
+
+// const root = ReactDOM.createRoot(
+//   document.getElementById('root') as HTMLElement
+// );
+// root.render(
+//   <React.StrictMode>
+//     <Home/>
+//   </React.StrictMode>
+// );
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Home from "./component/Home";
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter} from 'react-router-dom'; // Import BrowserRouter
 
+// Update the root element where you want to render your app
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <Home/>
+    {/* Wrap your component with BrowserRouter and set the basename */}
+    <BrowserRouter basename="/CRUD-Application">
+      <Home />
+    </BrowserRouter>
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
